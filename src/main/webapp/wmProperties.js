94176c3955440f0c1f6838de3432240063532736
Priyanka Pathoori <priyanka.pathoori@wavemaker.com>
2018-07-25 06:00:56 +0000

    updated source New files added - [Departments,EmployeeDetailDialog,EmployeeDetails,EmployeesView,Statistics,FileService,hrdb,googlemaps,Android,IOS,Main,Other,data,googlemaps,file_map_readme.txt,en.json,en.json,prefab-icon.png,ng-map.min.js,prefabPropertiesMetadata.json,6.png,Android,IOS,README.txt] Files modified - [Common,Login,Main,footer,header,leftnav,rightnav,topnav,securityService,WEB-INF,logos,profiles,resources,webapp,.gitignore,.wmproject.properties,pom.xml,pages-config.json] Removed files - [momentLocale,Readme.txt]

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "defaultLanguage" : "en",
+  "displayName" : "HRScaffold",
+  "homePage" : "Main",
+  "name" : "HRScaffold",
+  "platformType" : "WEB",
+  "securityEnabled" : "true",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0",
+  "xsrf_header_name" : "X-WM-XSRF-TOKEN"
+};
