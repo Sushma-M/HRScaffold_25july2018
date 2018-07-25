509dd6be772d6d1c78a568771d495c30dbc20e0d
Sushma Maddirala <sushma.maddirala@wavemaker.com>
2018-07-25 06:11:57 +0000

    Added lightbox and added images New files added - [Lightbox,hrdb.log] Files modified - [Main,googlemaps,app.variables.json]

---
+++ src/main/webapp/WEB-INF/prefabs/Lightbox/webapp/wmProperties.js
@@ -0,0 +1,15 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Lightbox",
+  "homePage" : "Main",
+  "name" : "Lightbox",
+  "platformType" : "DEFAULT",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "PREFAB",
+  "version" : "1.0",
+  "xsrf_header_name" : "X-WM-XSRF-TOKEN"
+};
